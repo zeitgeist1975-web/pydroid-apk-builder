1bb92cae4a48aa161aa99787b19dcff3670449d2
zeitgeist1975-web <zeitgeist1975@gmail.com>
2026-02-01 23:40:52 +0900

    Refactor to use Kivy for GUI application

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello APK Build")
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MyApp(App):
+    def build(self):
+        return Label(text="Hello APK Build")
+
+if __name__ == '__main__':
+    MyApp().run()
